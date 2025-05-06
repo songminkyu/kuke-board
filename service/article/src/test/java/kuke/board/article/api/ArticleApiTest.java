ba77d4f449c69935bc9f40a70835ece2c6d80c41
songminkyu <rabbircarrot@naver.com>
2025-05-07 00:40:09 +0900

    first commit

---
+++ service/article/src/test/java/kuke/board/article/api/ArticleApiTest.java
@@ -0,0 +1,94 @@
+package kuke.board.article.api;
+
+import kuke.board.article.service.request.ArticleCreateRequest;
+import kuke.board.article.service.request.ArticleUpdateRequest;
+import kuke.board.article.service.response.ArticlePageResponse;
+import kuke.board.article.service.response.ArticleResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class ArticleApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9000");
+
+    @Test
+    void createTest(){
+        ArticleResponse articleResponse = create(new ArticleCreateRequest("mk song", "my content", 1L, 1L));
+        System.out.println("articleResponse = " + articleResponse);
+    }
+
+    ArticleResponse create(ArticleCreateRequest request) {
+        return restClient.post()
+                .uri("/v1/articles")
+                .body(request)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void readTest(){
+        ArticleResponse response = read(177945412047552512L);
+        System.out.println("response = " + response);
+    }
+
+    ArticleResponse read(Long articleId) {
+        return restClient.get()
+                .uri("/v1/articles/{articleId}", articleId)
+                .retrieve()
+                .body(ArticleResponse.class);
+    }
+
+    @Test
+    void updateTest() {
+        update(177945412047552512L);
+        ArticleResponse response = read(177945412047552512L);
+        System.out.println("response = " + response);
+    }
+
+    void update(Long articleId) {
+        restClient.put()
+                .uri("/v1/articles/{articleId}", articleId)
+                .body(new ArticleUpdateRequest("hi 2", "my content 22"))
+                .retrieve();
+    }
+
+    @Test
+    void deleteTest() {
+        restClient.delete()
+                .uri("/v1/articles/{articleId}", 177945412047552512L)
+                .retrieve();
+    }
+
+    @Test
+    void readAllTest() {
+        ArticlePageResponse response = restClient.get()
+                .uri("/v1/articles?boardId=1&pageSize=30&page=50000")
+                .retrieve()
+                .body(ArticlePageResponse.class);
+
+        System.out.println("response.getArticleCount() = " + response.getArticleCount());
+
+        for(ArticleResponse article : response.getArticles()) {
+            System.out.println("article = " + article.getArticleId());
+        }
+    }
+
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleCreateRequest{
+        private String title;
+        private String content;
+        private Long writerId;
+        private Long boardId;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    static class ArticleUpdateRequest {
+        private String title;
+        private String content;
+    }
+}
---
+++ service/article/src/test/java/kuke/board/article/repository/ArticleRepositoryTest.java
@@ -0,0 +1,40 @@
+package kuke.board.article.repository;
+
+import kuke.board.article.entity.Article;
+import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@Slf4j
+@SpringBootTest
+class ArticleRepositoryTest {
+
+    @Autowired
+    ArticleRepository articleRepository;
+
+    @Test
+    void findAllTest() {
+        List<Article> articles = articleRepository.findAll(1L, 1499970L, 30L);
+        log.info("articles.size = {}", articles.size());
+
+        for(Article article : articles) {
+            log.info("article = {}", article);
+        }
+        assertThat(articles.size()).isEqualTo(30);
+    }
+
+    @Test
+    void countTest() {
+        Long count = articleRepository.count(1L, 10000L);
+        log.info("count = {}", count);
+        assertThat(count).isEqualTo(10000);
+    }
+
+}
